408dfc293e2fde45614e48768bed3ba59c0e9599
Lucas Morais <lucas.morais23ti@gmail.com>
2020-12-04 12:26:42 -0300

    update componentes

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Pub from './Components/mqttMessage.js';
 import Gauge from './Components/mqttGauge.js';
 import Maps from './Components/mqttMaps.js';
+import DadosProvider, {DadosContext} from './Components/mqttMessage.js';
+
+import App2 from './Components/app2.js';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={{marginTop:20, fontSize:20, color:'white'}}>Aplicativo em construção</Text>
-      <Gauge/>
-      <Pub/>
-      <Maps/>
+      <DadosProvider>
+        <Gauge/>
+        <Maps/>
+        <App2/>
+      </DadosProvider>
+
     </View>
   );
 };
